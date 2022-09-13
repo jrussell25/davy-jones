7cb2a751bc7b9522d9757ff7bc821faf5b15d5df
John Russell <johncrussell25@gmail.com>
2022-09-13 16:17:11 -0400

    Flesh out functionality of DeepSee class

--- davy-jones/davy_jones.py
+++ davy-jones/davy_jones.py
@@ -1,7 +1,80 @@
 from pyvisa import ResourceManager
+from pyvisa.resources.serial import SerialInstrument
+import serial
+from serial.tools import list_ports
+
+from .status_codes import state_defs, state_number
 
 
-class DeepSee()
-    def __init__(self):
+def port_map() -> None:
+    for p in list_ports.comports():
+        try:
+            s_test = serial.Serial(p.name)
+            s_test.close()
+            print(p)
+        except:
+            pass
 
-    
+
+class DeepSee:
+    def __init__(self, port_name: Optional[str] = "ASRL4::INSTR", **kwargs):
+        super().__init__(self, **kwargs)
+        self.rm = ResourceManager()
+        self.device = self._setup_device(port_name)
+
+        self.min_wavelength = int(self.device.query("wav:min?"))
+        self.max_wavelenth = int(self.devuce.query("wav:max?"))
+
+    def _setup_device(self, port_name: str) -> SerialInstrument:
+        device = ResourceManager().open_resource(port_name)
+        device.baud_rate = 115200
+        device.read_termination = "\n"
+        device.write_termination = "\n"
+        return device
+
+    def power_on(self, blocking: bool = True, verbose: bool = False) -> None:
+
+        status = self.get_status()
+
+        if status != 25:
+            raise ValueError(
+                f"DeepSee is not ready to turn on. Found status number {status}"
+            )
+        self.device.write("on")
+
+        if blocking:
+            count = 0
+            while True:
+                time.sleep(1)
+                count += 1
+                status = self.get_status()
+                if verbose:
+                    print(f"{count} s - {status=:03d}")
+
+                if status == 50:
+                    break
+
+    def get_status(self) -> int:
+        status = self.query("*stb?")
+        return state_number(status)
+
+    def set_wavelength(self, wav: int) -> None:
+        self.device.write(f"wav")
+
+    def open_pump_shutter(self):
+        self.device.write("shut 1")
+
+    def open_stokes_shutter(self):
+        self.device.write("irshut 1")
+
+    def close_pump_shutter(self):
+        self.device.write("shut 0")
+
+    def close_stokes_shutter(self):
+        self.device.write("irshut 0")
+
+    def get_shutter_state(self):
+        pump = int(self.device.query("shut?"))
+        stokes = int(self.device.query("irshut?"))
+        states = ["OPEN", "CLOSED"]
+        print(f"Pump shutter is {states[pump]} -- Stokes shutter is {states[stokes]}")
